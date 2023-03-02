a1b71a0599889b227a7d6e851868c0ce5258d636
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-03-02 22:53:49 +0530

    Sum factorial

---
+++ sum and factorial.py
@@ -0,0 +1,18 @@
+def sum(n):
+    s=0
+    for i in range(1,n+1):
+        s=s+i
+    return s
+
+def factorial(n):
+    s=1
+    for i in range(1,n+1):
+        s=s*i
+    return s
+
+p=sum(3)
+q=factorial(6)
+print('p=',p,'q=',q)  
+                                                                        
+    
+    
